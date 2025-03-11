031692197b337fd2c0fc3c8d6448465bcde843d1
Haowei Wen <yushijinhun@gmail.com>
2025-03-11 14:28:31 +0800

    lego brick generation

---
+++ extension/source/lego_assemble/lego_assemble/brick_generator.py
@@ -0,0 +1,35 @@
+import omni.physx.scripts.utils as physx_utils
+from pxr import UsdGeom, Usd
+
+brick_length = 0.008    # 8 mm per stud
+plate_height = 0.0032   # 3.2 mm per plate
+stud_diameter = 0.0048  # 4.8 mm stud diameter
+stud_height   = 0.0017  # 1.7 mm stud height
+
+def create_brick(stage: Usd.Stage, path: str, length_studs=4, width_studs=2, height_plates=3, color=(0.3, 0.3, 0.3)):
+    length = length_studs * brick_length
+    width  = width_studs * brick_length
+    height = height_plates * plate_height
+
+    brick: UsdGeom.Xform = UsdGeom.Xform.Define(stage, path)
+    physx_utils.setPhysics(brick.GetPrim(), kinematic=False)
+
+    body: UsdGeom.Cube = UsdGeom.Cube.Define(stage, f"{path}/Body")
+    body.CreateSizeAttr(1.0)
+    UsdGeom.XformCommonAPI(body).SetScale((length, width, height))
+    UsdGeom.XformCommonAPI(body).SetTranslate((0, 0, height/2))
+    physx_utils.setCollider(body.GetPrim())
+    body.CreateDisplayColorAttr([color])
+
+    for i in range(length_studs):
+        for j in range(width_studs):
+            stud: UsdGeom.Cylinder = UsdGeom.Cylinder.Define(stage, f"{path}/Stud_{i}_{j}")
+            stud.CreateHeightAttr(1.0)
+            x_offset = (i - (length_studs-1)/2) * brick_length
+            y_offset = (j - (width_studs-1)/2) * brick_length
+            z_offset = height + stud_height/2
+            UsdGeom.XformCommonAPI(stud).SetTranslate((x_offset, y_offset, z_offset))
+            UsdGeom.XformCommonAPI(stud).SetScale((stud_diameter/2, stud_diameter/2, stud_height))
+            stud.CreateDisplayColorAttr([color])
+
+    return brick
--- extension/source/lego_assemble/lego_assemble/ui_extension_example.py
+++ extension/source/lego_assemble/lego_assemble/ui_extension_example.py
@@ -1,14 +1,24 @@
 import omni.ext
 import omni.ui
+import omni.usd
+from pxr import Usd, UsdGeom
+from .brick_generator import create_brick
 
 class ExampleExtension(omni.ext.IExt):
+
+    def on_clicked(self):
+        stage: Usd.Stage = omni.usd.get_context().get_stage()
+        if stage is not None:
+            stage.RemovePrim("/World/Brick")
+            brick = create_brick(stage, "/World/Brick")
+            UsdGeom.XformCommonAPI(brick).SetTranslate((0, 0, 0.100))
 
     def on_startup(self, ext_id):
         print("[lego_assemble] startup")
 
         self._window = omni.ui.Window("My Window", width=300, height=300)
         with self._window.frame:
-            omni.ui.Button("Click me", clicked_fn=lambda: print("[lego_assemble] Hello world!"))
+            omni.ui.Button("Click me", clicked_fn=self.on_clicked)
 
     def on_shutdown(self):
         print("[lego_assemble] shutdown")
