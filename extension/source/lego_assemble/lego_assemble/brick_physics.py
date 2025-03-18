064cacd22fef2245cfa28cc7abb99e430dacd29e
Haowei Wen <yushijinhun@gmail.com>
2025-03-19 02:46:06 +0800

    fix math

--- extension/source/lego_assemble/lego_assemble/brick_physics.py
+++ extension/source/lego_assemble/lego_assemble/brick_physics.py
@@ -1,4 +1,3 @@
-import math
 import logging
 import omni.usd
 import omni.physx
@@ -11,9 +10,9 @@ from . import lego_schemes
 
 DistanceTolerance = 0.001           # Maximum distance between bricks (m)
 MaxPenetration = 0.005              # Maximum penetration between bricks (m), penetration can happen due to simulation inaccuracies
-ZAngleTolerance = math.radians(5)   # Maximum angle between z-axis of bricks (rad)
-RequiredForce = 1.0                 # Minimum clutch power (N)
-YawTolerance = math.radians(5)      # Maximum yaw error (rad)
+ZAngleTolerance = np.radians(5)     # Maximum angle between z-axis of bricks (rad)
+RequiredForce = 0.1                 # Minimum clutch power (N)
+YawTolerance = np.radians(5)        # Maximum yaw error (rad)
 PositionTolerance = 0.002           # Maximum position error (m)
 
 logger = logging.getLogger(__name__)
@@ -57,21 +56,21 @@ def _contact_report_event_handler(contact_headers: ContactEventHeaderVector, con
         dim_attr1 = prim1.GetAttribute("lego_dimensions")
         if not dim_attr0 or not dim_attr1:
             continue
-        dim0: Gf.Vec3i = np.array(dim_attr0.Get())
-        dim1: Gf.Vec3i = np.array(dim_attr1.Get())
+        dim0 = np.array(dim_attr0.Get(), dtype=int)
+        dim1 = np.array(dim_attr1.Get(), dtype=int)
 
-        pose0 = omni.usd.get_world_transform_matrix(prim0)
-        pose1 = omni.usd.get_world_transform_matrix(prim1)
-        rel_pose: Gf.Matrix4d = pose0.GetInverse() * pose1
-        rel_z = rel_pose.ExtractTranslation()[2]
+        pose0 = np.array(omni.usd.get_world_transform_matrix(prim0)).T
+        pose1 = np.array(omni.usd.get_world_transform_matrix(prim1)).T
+        rel_pose = np.linalg.inv(pose0) @ pose1
+        rel_z = rel_pose[2,3]
 
         # Swap the order of the bricks, so prim0 is always the one offering studs
         if rel_z < 0:
             prim0, prim1 = prim1, prim0
             dim0, dim1 = dim1, dim0
             pose0, pose1 = pose1, pose0
-            rel_pose: Gf.Matrix4d = pose0.GetInverse() * pose1
-            rel_z = rel_pose.ExtractTranslation()[2]
+            rel_pose = np.linalg.inv(pose0) @ pose1
+            rel_z = rel_pose[2,3]
         # Delete other variables so we don't accidentally use them
         del actor0, actor1, dim_attr0, dim_attr1
 
@@ -84,24 +83,24 @@ def _contact_report_event_handler(contact_headers: ContactEventHeaderVector, con
 
         # The angle between the z-axis of the first brick and the z-axis of the second brick
         # Must be within a threshold to triger assembly
-        rel_z_angle = math.acos(rel_pose[2,2])
+        rel_z_angle = np.arccos(rel_pose[2,2])
 
         impulse = np.zeros(3)
         contact_pt: ContactData
         for contact_pt in contact_data[contact.contact_data_offset:contact.contact_data_offset+contact.num_contact_data]:
             impulse += contact_pt.impulse
         # The contact force along the z-axis of the first brick
-        frc_prj = np.abs(np.dot(impulse, np.array(pose0)[:3,2])) / sim_timestep
+        frc_prj = np.abs(np.dot(impulse, pose0[:3,2])) / sim_timestep
 
         # Angle between the x-axis of the first brick and the x-axis of the second brick
-        rel_yaw = math.atan2(rel_pose[1,0], rel_pose[0,0])
+        rel_yaw = np.arctan2(rel_pose[1,0], rel_pose[0,0])
         # Expected angle after assembly, can be -pi, -pi/2, 0, pi/2, pi
-        snapped_yaw = round(rel_yaw / (math.pi/2)) * (math.pi/2)
+        snapped_yaw = np.round(rel_yaw / (np.pi/2)) * (np.pi/2)
         # Error withing [-pi/4, pi/4]
         yaw_err = rel_yaw - snapped_yaw
 
         # Relative position of the second brick in studs
-        p0 = np.array(rel_pose.ExtractTranslation()[:2]) / lego_schemes.BrickLength + (dim0[:2] - np.array(rel_pose)[:2,:2] @ dim1[:2]) / 2 
+        p0 = rel_pose[:2,3] / lego_schemes.BrickLength + (dim0[:2] - rel_pose[:2,:2] @ dim1[:2]) / 2 
         p0_snapped = np.round(p0).astype(int)
         R_snapped = np.array([[np.cos(snapped_yaw), -np.sin(snapped_yaw)], [np.sin(snapped_yaw), np.cos(snapped_yaw)]])
         p1_snapped = np.round(p0 + R_snapped @ dim1[:2]).astype(int)
@@ -112,12 +111,14 @@ def _contact_report_event_handler(contact_headers: ContactEventHeaderVector, con
         overlap_area = overlap_x * overlap_y
 
         assemble_xy = (p0_snapped + (R_snapped @ dim1[:2] - dim0[:2]) / 2) * lego_schemes.BrickLength
-        assemble_tr = Gf.Matrix4d().SetTransform(
-            Gf.Rotation().SetAxisAngle(Gf.Vec3d(0, 0, 1), snapped_yaw),
-            Gf.Vec3d(assemble_xy[0], assemble_xy[1], height0)
-        )
-        parent_pose1: Gf.Matrix4d = UsdGeom.Xformable(prim1).ComputeParentToWorldTransform(Usd.TimeCode.Default())
-        desired_tr: Gf.Matrix4d = parent_pose1.GetInverse() * pose0 * assemble_tr
+        assemble_tr = np.array([
+            [R_snapped[0,0], R_snapped[0,1], 0, assemble_xy[0]  ],
+            [R_snapped[1,0], R_snapped[1,1], 0, assemble_xy[1]  ],
+            [0,              0,              1, height0         ],
+            [0,              0,              0, 1               ],
+        ])
+        parent_pose1= np.array(UsdGeom.Xformable(prim1).ComputeParentToWorldTransform(Usd.TimeCode.Default())).T
+        desired_tr = np.linalg.inv(parent_pose1) @ pose0 @ assemble_tr
 
         if rel_distance > DistanceTolerance:
             status = "exceeding distance tolerance"
@@ -134,8 +135,8 @@ def _contact_report_event_handler(contact_headers: ContactEventHeaderVector, con
         elif frc_prj < RequiredForce:
             status = "insufficient force"
         else:
-            physicsUtils.set_or_add_translate_op(prim1, desired_tr.ExtractTranslation())
-            physicsUtils.set_or_add_orient_op(prim1, desired_tr.ExtractRotation().GetQuat())
+            physicsUtils.set_or_add_translate_op(prim1, desired_tr[:3,3].tolist())
+            physicsUtils.set_or_add_orient_op(prim1, Gf.Matrix3d(desired_tr[:3,:3].T).ExtractRotation().GetQuat())
             joint_path = f"{prim0.GetPath()}_Conn_{prim1.GetPath().name}"
             if stage.GetPrimAtPath(joint_path).IsValid():
                 status = "already assembled"
@@ -149,12 +150,12 @@ def _contact_report_event_handler(contact_headers: ContactEventHeaderVector, con
             f"Contact between {prim0.GetPath()} ({dim0[0]}x{dim0[1]}x{dim0[2]}) "
             f"and {prim1.GetPath()} ({dim1[0]}x{dim1[1]}x{dim1[2]}), "
             f"distance={rel_distance:.3f} m, "
-            f"z-angle={math.degrees(rel_z_angle):.3f} deg, "
+            f"z-angle={np.degrees(rel_z_angle):.3f} deg, "
             f"force={frc_prj:.3f} N, "
             f"p_0=({p0_snapped[0]}, {p0_snapped[1]}), "
             f"p_1=({p1_snapped[0]}, {p1_snapped[1]}), "
             f"p_err=({p_err[0]:.3f}, {p_err[1]:.3f}), "
-            f"rel_yaw={math.degrees(snapped_yaw):.3f} ({math.degrees(yaw_err):.3f}) deg, "
+            f"rel_yaw={np.degrees(snapped_yaw):.3f} ({np.degrees(yaw_err):.3f}) deg, "
             f"overlap={overlap_x}x{overlap_y}, "
             f"status: {status}"
         )
