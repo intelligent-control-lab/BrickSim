cb21fd11817854deff2f4b30af09827d6008d1e8
waynekyrie <ruixuanl@andrew.cmu.edu>
2026-01-27 16:01:34 -0500

    create folder when downloading lego dataset

--- scripts/download_legosim_dataset.sh
+++ scripts/download_legosim_dataset.sh
@@ -6,5 +6,7 @@ ROOT_DIR=$(cd -- "$SCRIPT_DIR/.." && pwd -P)
 
 DATASET_URL="https://www.cs.cmu.edu/~haoweiw/legosim_dataset.tar.xz"
 
-cd "$ROOT_DIR/resources/legosim_dataset"
+DOWNLOAD_DIR="$ROOT_DIR/resources/legosim_dataset"
+mkdir -p "$DOWNLOAD_DIR"
+cd "$DOWNLOAD_DIR"
 wget -O- "$DATASET_URL" | tar -xJf -
